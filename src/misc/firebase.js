9ce633baf635719ba728def1693a2ab8a0d8fbae
Christine Doza <cdoza@leoevents.com>
2021-10-26 14:02:29 -0500

    firebase initialization

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app"
+
+const config = {
+    apiKey: "AIzaSyAo3lle-R0cr8Od6yIFrUza7TK0JoMjnMo",
+    authDomain: "chat-web-app-ab1a1.firebaseapp.com",
+    projectId: "chat-web-app-ab1a1",
+    storageBucket: "chat-web-app-ab1a1.appspot.com",
+    messagingSenderId: "652623501522",
+    appId: "1:652623501522:web:d7338baa70bf9764a1b50b"
+  };
+
+  const app = firebase.initializeApp(config);
