addb6d76d52ba5a796e66da460df04dee9981c00
Christine Doza <cdoza@leoevents.com>
2021-10-27 11:09:14 -0500

    manage user with profile context

--- src/components/PublicRoute.js
+++ src/components/PublicRoute.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import { Redirect, Route } from 'react-router';
+import { Container, Loader } from 'rsuite';
 import { useProfile } from '../context/profile.context';
 
 const PublicRoute = ({ children, ...routeProps }) => {
 
-    const profile = useProfile();
+    const {profile, isLoading} = useProfile();
+
+    if(isLoading && !profile){
+        return <Container>
+            <Loader center vertical size="md" content="Loading" speed="slow"  />
+        </Container>
+    }
     //if a profile DOES exist, redirect from sign-in to Home page
-    if(profile) {
+    if(profile && !isLoading) {
         return <Redirect to="/" />
     }
+    
     
     return (
         <Route {...routeProps}>
