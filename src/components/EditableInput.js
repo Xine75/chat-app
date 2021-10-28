fbaf5d6cd92a0584de2b82ec65fa455e7afaa24b
Christine Doza <cdoza@leoevents.com>
2021-10-28 11:24:04 -0500

    save nickname change to db

--- src/components/EditableInput.js
+++ src/components/EditableInput.js
@@ -24,7 +24,8 @@ const EditableInput = ({initialValue,
     const onSaveClick = async() => {
         const trimmed = input.trim();
         if (trimmed === ""){
-            Alert.info(emptyMsg, 4000)
+            Alert.info(emptyMsg, 4000);
+            return;
         }
         if (trimmed !== initialValue){
             await onSave(trimmed)
