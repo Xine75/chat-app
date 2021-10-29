5904fc7a57af48ec76373d21bff4341ea3538811
Christine Doza <cdoza@leoevents.com>
2021-10-29 15:39:56 -0500

    installed context-selector and created room context

--- src/pages/Home/Chat.js
+++ src/pages/Home/Chat.js
@@ -4,28 +4,31 @@ import { Loader } from 'rsuite';
 import ChatBottom from '../../components/chat-window/bottom'
 import Messages from '../../components/chat-window/messages'
 import ChatTop from '../../components/chat-window/top';
+import { CurrentRoomProvider } from '../../context/current-room.context';
 import {useRooms} from "../../context/rooms.context";
 
 const Chat = () => {
 
     const {chatId} = useParams();
-
     const rooms = useRooms();
 
     if(!rooms){
         return <Loader center vertical sz="md" content="Loading" speed="slow" />
     }
-
     const currentRoom = rooms.find(room => room.id === chatId);
 
     if (!currentRoom){
         return <h6 className="text-center mt-page">Chat {chatId} not found</h6>;
-    }
+    };
 
+    const {name, description} = currentRoom;
+
+    const currentRoomData = {
+        name, description
+    } 
 
     return (
-        <>
-            
+        <CurrentRoomProvider data={currentRoomData}>     
             <div className="chat-top">
                 <ChatTop />
             </div>
@@ -37,8 +40,7 @@ const Chat = () => {
             <div className="chat-bottom">
                 <ChatBottom />
             </div>
-        </>
+        </CurrentRoomProvider>
     )
 }
-
 export default Chat
