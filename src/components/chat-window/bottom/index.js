fa69f2f3696921b8c2cc6e0208e2fb40db951071
Christine Doza <cdoza@leoevents.com>
2021-11-01 10:35:39 -0500

    created chat bottom to connect with firebase

--- src/components/chat-window/bottom/index.js
+++ src/components/chat-window/bottom/index.js
@@ -1,10 +1,88 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
+import { InputGroup, Input, Icon, Alert } from 'rsuite';
+import firebase from 'firebase/app';
+import { useParams } from 'react-router-dom';
+import { useProfile } from '../../../context/profile.context';
+import {database} from "../../../misc/firebase"
+
+function assembleMessage (profile, chatId) {
+    return{
+    roomId: chatId,
+    author: {
+        name: profile.name,
+        uid: profile.uid,
+        createdAt: profile.createdAt,
+        ...(profile.avatar ? { avatar: profile.avatar } : {})
+    },
+    createdAt: firebase.database.ServerValue.TIMESTAMP,
+    }
+}
 
 const ChatBottom = () => {
+
+    const [input, setInput] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const { profile } = useProfile();
+    const { chatId } = useParams();
+
+    const onInputChangeHandler = useCallback((value) => {
+        setInput(value)
+        },[]);
+
+    const onKeyDown = (ev) => {
+        if (ev.keyCode === 13){
+            ev.preventDefault();
+            onSendClick()
+        }
+    }
+
+    const onSendClick = async() => {
+        if(input.trim() === ""){
+            return;
+        }
+        const msgData = assembleMessage(profile, chatId);
+        msgData.text = input;
+
+        const updates = {};
+
+        const messageId = database.ref("messages").push().key;
+
+        updates[`/messages/${messageId}`] = msgData;
+        updates[`/rooms/${chatId}/lastMessage`] = {
+            ...msgData,
+            msgId: messageId
+        };
+        setIsLoading(true)
+        try {
+            await database.ref().update(updates);
+            setInput("");
+            setIsLoading(false);
+        } catch (err) {
+            setIsLoading(false)
+            Alert.error(err.message)
+        };
+ };
+
+
     return (
         <div>
-            ChatBottom
-        
+            <InputGroup>
+            <Input 
+            placeholder="Write a new message here..."
+            value={input}
+            onChange={onInputChangeHandler}
+            onKeyDown={onKeyDown}
+            />
+
+            <InputGroup.Button 
+            color="blue" 
+            appearance="primary"
+            onClick={onSendClick}
+            disabled={isLoading}>
+                <Icon icon="send" />
+
+            </InputGroup.Button>
+            </InputGroup>        
         </div>
     )
 }
