499f108a5b3f2ea457837133c5a9ef220c920a9a
Christine Doza <cdoza@leoevents.com>
2021-11-09 15:01:29 -0600

    firebase interactions

--- src/misc/helpers.js
+++ src/misc/helpers.js
@@ -1,3 +1,5 @@
+import { ref, get, query, orderByChild, equalTo } from 'firebase/database';
+
 
 export function getNameInitials(name) {
 
@@ -15,24 +17,22 @@ export function tranformToArrayWithId(snapVal) {
     }) : []
 }
 
-
 export async function getUserUpdates(userId, keyToUpdate, value, db) {
     const updates = {};
   
     updates[`/profiles/${userId}/${keyToUpdate}`] = value;
   
-    const getMsgs = db
-        .ref("/messages")
-        .orderByChild("author/uid")
-        .equalTo(userId)
-        .once("value");
-   
+    const getMsgs = get(
+      query(ref(db, '/messages'), orderByChild('author/uid'), equalTo(userId))
+    );
   
-    const getRooms = db
-        .ref("/rooms")
-        .orderByChild("lastMessage/author/uid")
-        .equalTo(userId)
-        .once(value);
+    const getRooms = get(
+      query(
+        ref(db, '/rooms'),
+        orderByChild('lastMessage/author/uid'),
+        equalTo(userId)
+      )
+    );
     // Index not defined, add ".indexOn": "author/uid", for path "/messages", to the rules
   
     const [mSnap, rSnap] = await Promise.all([getMsgs, getRooms]);
@@ -47,3 +47,36 @@ export async function getUserUpdates(userId, keyToUpdate, value, db) {
   
     return updates;
   }
+
+
+// export async function getUserUpdates(userId, keyToUpdate, value, db) {
+//     const updates = {};
+  
+//     updates[`/profiles/${userId}/${keyToUpdate}`] = value;
+  
+//     const getMsgs = db
+//         .ref("/messages")
+//         .orderByChild("author/uid")
+//         .equalTo(userId)
+//         .once("value");
+   
+  
+//     const getRooms = db
+//         .ref("/rooms")
+//         .orderByChild("lastMessage/author/uid")
+//         .equalTo(userId)
+//         .once(value);
+//     // Index not defined, add ".indexOn": "author/uid", for path "/messages", to the rules
+  
+//     const [mSnap, rSnap] = await Promise.all([getMsgs, getRooms]);
+  
+//     mSnap.forEach(msgSnap => {
+//       updates[`/messages/${msgSnap.key}/author/${keyToUpdate}`] = value;
+//     });
+  
+//     rSnap.forEach(roomSnap => {
+//       updates[`/rooms/${roomSnap.key}/lastMessage/author/${keyToUpdate}`] = value;
+//     });
+  
+//     return updates;
+//   }
